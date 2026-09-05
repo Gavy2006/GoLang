b30417bb28f65c4f4bff85d9cf0745d585795f3e
Gavy2006 <gavygaba13@gmail.com>
2026-09-05 11:39:11 +0530

    basic modules and var done

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("learn go language")
+
+	printMsg()
+
+	var name string = "Gavy"
+	fmt.Println(name)
+
+	var age int = 20
+	fmt.Println(age)
+
+	var check bool = true
+	fmt.Println(check)
+
+	var money float32 = 902002.32
+	fmt.Print(money)
+
+	fmt.Println("money", money)
+
+}
