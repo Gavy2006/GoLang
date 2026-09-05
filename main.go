bd05c0dcd1235e959d2a629a74e3def794fd49c5
Gavy2006 <gavygaba13@gmail.com>
2026-09-05 11:56:44 +0530

    var done

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+var Public = "it is public" // can be access from outside
+
+// agr kisi variable ko bahr se access krna hai to us var ke name ko capital mei rkhan pdega not in small public -> Public
+
+// same applicable for func names also
+//PublicFunction() -> can access from diff packages
+//publicFunction() -> can not access from diff packages
+
 func main() {
 
 	fmt.Println("learn go language")
@@ -18,8 +26,30 @@ func main() {
 	fmt.Println(check)
 
 	var money float32 = 902002.32
-	fmt.Print(money)
+	fmt.Println(money)
 
-	fmt.Println("money", money)
+	fmt.Println("money =>>", money)
+
+	const pii = 67.12
+	fmt.Println(pii)
+
+	person := 123
+	fmt.Println(person)
+
+	user := "gavy"
+	fmt.Println(user)
+
+	// user = 90 error *** samae data type
+	user = "545"
+	fmt.Println(user)
+
+	person = 90
+	var public = "it is public"
+	var private = "it is private"
+
+	fmt.Println(public)
+	fmt.Println(private)
+
+	fmt.Println(Public)
 
 }
--- myutil.go
+++ myutil.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func printMsg() {
 	fmt.Println("hello")
+
+	fmt.Println(Public)
+
 }
